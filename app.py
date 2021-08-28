43bf6fcfec85c6db90b6a178344603f8d622cfae
James Meixner <68332274+zocrateez@users.noreply.github.com>
2021-08-28 22:45:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -32,11 +32,10 @@ L_m, U, s_t = symbols("L_m,U,sbar")
 Pi_m = E/(L1*(1-s_t)+L*s_t)
 
 L_m = L1*(1-Pi_m*s_t) + L*Pi_m*s_t
-L_m_sol = L_m.subs(L1,L1_sol)
+L_m_sol = L_m.subs(L1,solve(endog,L1)[0])
 L_m_sol_func = lambdify([m,a,E,L,s_t],L_m_sol,"numpy")
 
 UR = 1-(E/L_m_sol)
-UR
 unemployment_rate_func = lambdify([m,a,E,L,s_t],UR,"numpy")
 
 #FB
